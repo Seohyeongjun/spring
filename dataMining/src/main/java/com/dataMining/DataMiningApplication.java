f10c2a37a42efad38da407d28e3111c5efba3092
Seohyeongjun <seo.hyeongjun@gmail.com>
2024-08-14 01:38:30 +0900

    dataMining

---
+++ dataMining/src/main/java/com/dataMining/DataMiningApplication.java
@@ -0,0 +1,13 @@
+package com.dataMining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMiningApplication.class, args);
+	}
+
+}
